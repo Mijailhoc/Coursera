ed72d2900e6ff04de1dcc66421b8af906d9ed6fd
Mijailhoc <mijailorozco@gmail.com>
2024-08-17 13:39:41 -0500

    https://github.com/Mijailhoc/Coursera.git
    Please use this link to access de info...

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objetivos:**
+# - Listar lenguajes populares para Ciencia de Datos
+# - Identificar herramientas clave de código abierto
+# - Comprender el rol de los entornos de desarrollo
+# - Explorar flujos de trabajo comunes en Ciencia de Datos
+# - Reconocer la importancia de herramientas colaborativas
+
+# ### Some of the popular languages that Data Scientists use are:
+
+# #### 
+# 1. Python
+# 2. R
+# 3. SQL
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+
+# ####
+# 1. Numpy
+# 2. Pandas
+# 3. Matplotlib
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | RStudio            |
+# | Apache Zeppelin    |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+# 
+# 1. a == 1 + 2
+# 2. b == a / 1
+# 3. c == b * 5
+
+# In[4]:
+
+
+"This a simple arithmetic expression to mutiply then add integers"
+(3*4)+5
+
+
+# In[5]:
+
+
+"This will convert 200 minutes to hours by diving by 60"
+200 / 60
+
+
+# ## Author
+# Mijail Orozco
